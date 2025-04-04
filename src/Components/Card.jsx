9ae60ab102adafcc004cfcf30d2b80356af7cef7
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-04-04 17:38:48 +0100

    Added button flip effect and increment button functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
 import styles from '../Styles/Card.module.css'
 function Card(){
+    const [flipped, setFlipped] = useState(false);
+    const [quantity, setQuantity] = useState(0);
+    function handleFlip(){
+        setFlipped(true);
+        handleAdd();
+    }
+    const handleAdd = () => setQuantity(quantity + 1);
+    function handleDecrease(){
+        if(quantity > 0){
+            setQuantity(quantity - 1);
+        }
+    }
+
     return (
       <div className={styles.card}>
         <div className={styles.img}>
@@ -7,11 +21,13 @@ function Card(){
         </div>
         <div className={styles.info}>
             <h2>Title</h2>
-            <button>Add to Cart</button>
-            <div className={styles.add}>
-                <button className={styles.btns} >+</button>
-                <input type="text" className={styles.input} />
-                <button className={styles.btns} >-</button>
+            <div className={styles.flip}>
+                <button onClick={handleFlip}>Add to Cart</button>
+                <div className={`${styles.add} ${flipped ? styles.front : styles.back}`}>
+                    <button className={styles.btns} onClick={handleAdd}>+</button>
+                    <input type="text" className={styles.input} value={quantity} />
+                    <button className={styles.btns} onClick={handleDecrease}>-</button>
+                </div>
             </div>
         </div>
       </div>
---
+++ src/Components/Tile.jsx
@@ -0,0 +1,12 @@
+function Tile(){
+    return(
+        <div>
+            <div className="img"></div>
+            <div className="deets">
+                <h2></h2>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    )
+}
